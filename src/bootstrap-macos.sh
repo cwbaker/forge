6c7a20c80733e0cb743cee66112061190909232c
Charles Baker <charles.baker@sweetsoftware.co.nz>
2017-12-26 15:28:10 +1300

    Update bootstrap script for macOS

--- src/bootstrap-macos.sh
+++ src/bootstrap-macos.sh
@@ -1,12 +1,12 @@
 
 SRC=$(pwd)
-LIB=$(pwd)/../bootstrap/lib
-BIN=$(pwd)/../bootstrap/bin
+LIB=$(pwd)/../bootstrap-macos/lib
+BIN=$(pwd)/../bootstrap-macos/bin
 
 cc() {
     for file in $1; do
         echo $file...
-        local DEFINES="-DBUILD_OS_MACOSX -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
+        local DEFINES="-DBUILD_OS_MACOS -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
         local INCLUDE_DIRS="-I $SRC -I $SRC/lua -I $SRC/boost"
         local FLAGS="-x c -g"
         clang $DEFINES $INCLUDE_DIRS $FLAGS -o $file.o -c $file
@@ -16,7 +16,7 @@ cc() {
 cxx() {
     for file in $1; do
         echo $file...
-        local DEFINES="-DBUILD_OS_MACOSX -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
+        local DEFINES="-DBUILD_OS_MACOS -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
         local INCLUDE_DIRS="-I $SRC -I $SRC/lua -I $SRC/boost"
         local FLAGS="-x c++ -std=c++11 -stdlib=libc++ -fexceptions -frtti -g"
         clang++ $DEFINES $INCLUDE_DIRS $FLAGS -o $file.o -c $file
@@ -28,7 +28,7 @@ archive() {
 }
 
 link() {
-    clang++ *.o -g -L $LIB -lassert -lcmdline -lpersist -lrtti -lbuild_tool -lbuild_tool_lua -lprocess -lsweetlua -lerror -llua -lboost_filesystem -lboost_system -o $1
+    clang++ *.o -g -L $LIB -lassert -lcmdline -lbuild_tool -lbuild_tool_lua -lprocess -lluaxx -lerror -llua -lboost_filesystem -lboost_system -o $1
 }
 
 mkdir -p $LIB
@@ -40,10 +40,8 @@ echo sweet/build_tool; pushd sweet/build_tool; cxx '*.cpp'; archive $LIB/libbuil
 echo sweet/build_tool/build_tool_lua; pushd sweet/build_tool/build_tool_lua; cxx '*.cpp'; archive $LIB/libbuild_tool_lua.a; popd
 echo sweet/cmdline; pushd sweet/cmdline; cxx '*.cpp'; archive $LIB/libcmdline.a; popd
 echo sweet/error; pushd sweet/error; cxx '*.cpp'; archive $LIB/liberror.a; popd
-echo sweet/lua; pushd sweet/lua; cxx '*.cpp'; archive $LIB/libsweetlua.a; popd
-echo sweet/persist; pushd sweet/persist; cxx '*.cpp'; archive $LIB/libpersist.a; popd
+echo sweet/luaxx; pushd sweet/luaxx; cxx '*.cpp'; archive $LIB/libluaxx.a; popd
 echo sweet/process; pushd sweet/process; cxx '*.cpp'; archive $LIB/libprocess.a; popd
-echo sweet/rtti; pushd sweet/rtti; cxx '*.cpp'; archive $LIB/librtti.a; popd
 
 mkdir -p $BIN
 echo sweet/build_tool/build; pushd sweet/build_tool/build; cxx '*.cpp'; link $BIN/build; popd
