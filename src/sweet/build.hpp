2da270e98cac0d9f1162d090d34daac64bcae5bc
Charles Baker <charles.baker@sweetsoftware.co.nz>
2017-12-26 13:08:49 +1300

    Remove default library suffix macro from build.hpp

--- src/sweet/build.hpp
+++ src/sweet/build.hpp
@@ -1,27 +1,10 @@
 //
 // build.hpp
-// Copyright (c) 2008 - 2015 Charles Baker.  All rights reserved.
+// Copyright (c) Charles Baker. All rights reserved.
 //
 
 #ifndef SWEET_BUILD_HPP_INCLUDED
 #define SWEET_BUILD_HPP_INCLUDED
-
-//
-// If compiling for Windows and the BUILD_LIBRARY_SUFFIX macro isn't defined 
-// assume that the Sweet Build System is not being used to compile whatever 
-// is including this file and make a guess at the correct values for 
-// BUILD_LIBRARY_SUFFIX and which variant is being compiled based on whether 
-// or not the _DEBUG macro is defined.
-//
-#if defined BUILD_OS_WINDOWS && !defined BUILD_LIBRARY_SUFFIX
-#ifdef _DEBUG
-#define BUILD_VARIANT_DEBUG
-#define BUILD_LIBRARY_SUFFIX "_msvc_debug.lib"
-#else
-#define BUILD_VARIANT_RELEASE
-#define BUILD_LIBRARY_SUFFIX "_msvc_release.lib"
-#endif
-#endif
 
 //
 // Define macros for all components based on the variant that is being 
