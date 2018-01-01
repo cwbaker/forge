31d50a45a2e659eebe09e6fddcf31d265f535bd1
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-01-01 21:30:20 +1300

    Update `Copy` target to build to relative path instead of object path

--- src/sweet/build_tool/lua/build/Copy.lua
+++ src/sweet/build_tool/lua/build/Copy.lua
@@ -1,6 +1,13 @@
 
 local Copy = build:TargetPrototype( "Copy" );
 
+function Copy.create( build, settings, identifier )
+    local copy = build:File( identifier, Copy );
+    copy:add_ordering_dependency( build:Directory(copy:branch()) );
+    copy.settings = settings;
+    return copy;
+end
+
 function Copy.build( build, target )
     build:rm( target );
     build:cp( target, target:dependency() );
