b23e71306d07aeca2b02c4aa07a4ab779742051f
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-04-28 21:39:42 +1200

    Move build.lua up from src/ to the root directory

--- build.lua
+++ build.lua
@@ -2,7 +2,7 @@
 -- Reference Lua build scripts from within the source tree rather than 
 -- relative to the build executable so that they can be edited in place during
 -- development and are versioned along with the code that they are building.
-package.path = build:root('sweet/build_tool/lua/?.lua')..';'..build:root('sweet/build_tool/lua/?/init.lua');
+package.path = build:root('src/sweet/build_tool/lua/?.lua')..';'..build:root('src/sweet/build_tool/lua/?/init.lua');
 
 require "build";
 require "build.cc";
@@ -20,22 +20,22 @@ jobs = jobs or 4;
 
 local settings = build:initialize {
     variants = { "debug", "release", "shipping" };
-    bin = build:root( ("../%s/bin"):format(variant) );
-    lib = build:root( ("../%s/lib"):format(variant) );
-    lua = build:root( ("../%s/lua"):format(variant) );
-    obj = build:root( ("../%s/obj"):format(variant) );
+    bin = build:root( ('%s/bin'):format(variant) );
+    lib = build:root( ('%s/lib'):format(variant) );
+    lua = build:root( ('%s/lua'):format(variant) );
+    obj = build:root( ('%s/obj'):format(variant) );
     include_directories = {
-        build:root(),
-        build:root( "boost" )
+        build:root( 'src' ),
+        build:root( 'src/boost' )
     };
     library_directories = {
-        build:root( ("../%s/lib"):format(variant) ),
+        build:root( ('%s/lib'):format(variant) ),
     };
     visual_studio = {
-        sln = build:root( "../sweet_build.sln" );
+        sln = build:root( "sweet_build.sln" );
     };
     xcode = {
-        xcodeproj = build:root( "../sweet_build.xcodeproj" );
+        xcodeproj = build:root( "sweet_build.xcodeproj" );
     };
     zero_brane_studio = {
         mobdebug = build:switch { build:operating_system();
@@ -48,20 +48,20 @@ local settings = build:initialize {
 -- Targets built when building from the root directory and as targets when
 -- generating XCode projects and Visual Studio solutions.
 build:default_targets {
-    "sweet/build_tool",
-    "sweet/build_tool/build",
-    "sweet/build_tool/build_hooks"
+    'src/sweet/build_tool',
+    'src/sweet/build_tool/build',
+    'src/sweet/build_tool/build_hooks'
 };
 
-buildfile "boost/boost.build";
-buildfile "lua/lua.build";
-buildfile "sweet/assert/assert.build";
-buildfile "sweet/build_tool/build_tool.build";
-buildfile "sweet/cmdline/cmdline.build";
-buildfile "sweet/error/error.build";
-buildfile "sweet/luaxx/luaxx.build";
-buildfile "sweet/process/process.build";
-buildfile "unit/unit.build";
+buildfile 'src/boost/boost.build';
+buildfile 'src/lua/lua.build';
+buildfile 'src/sweet/assert/assert.build';
+buildfile 'src/sweet/build_tool/build_tool.build';
+buildfile 'src/sweet/cmdline/cmdline.build';
+buildfile 'src/sweet/error/error.build';
+buildfile 'src/sweet/luaxx/luaxx.build';
+buildfile 'src/sweet/process/process.build';
+buildfile 'src/unit/unit.build';
 
 function install()
     prefix = prefix or build:home( 'sweet_build' );
