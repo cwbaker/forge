0fd948fc3c6b44e92fb5c75b01726e1518497a67
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-05-17 12:37:55 +1200

    Pass architecture through to dll_name() from DynamicLibrary

--- src/sweet/build_tool/lua/build/cc/DynamicLibrary.lua
+++ src/sweet/build_tool/lua/build/cc/DynamicLibrary.lua
@@ -1,23 +1,24 @@
 
 local DynamicLibrary = build:TargetPrototype( "DynamicLibrary" );
 
-local function default_identifier_filename( identifier, settings )
-    local settings = settings or build:current_settings();
+local function default_identifier_filename( settings, identifier, architecture )
     local identifier = build:absolute( build:interpolate(identifier, settings) );
     local basename = build:basename( identifier );
     local branch = build:branch( identifier );
-    local filename = ("%s/%s"):format( branch, settings.dll_name(basename) );
+    local filename = ("%s/%s"):format( branch, settings.dll_name(basename, architecture) );
     return identifier, filename;
 end
 
-function DynamicLibrary.create( build, settings, identifier )
-    local identifier, filename = default_identifier_filename( identifier, settings );
+function DynamicLibrary.create( build, settings, identifier, architecture )
+    local settings = settings or build:current_settings();
+    local architecture = architecture or settings.architecture or settings.default_architecture;
+    local identifier, filename = default_identifier_filename( settings, identifier, architecture );
     local dynamic_library = build:Target( identifier, DynamicLibrary );
     dynamic_library:set_filename( filename );
     dynamic_library:set_cleanable( true );
     dynamic_library:add_ordering_dependency( build:Directory(build:branch(dynamic_library)) );
     dynamic_library.settings = settings;
-    dynamic_library.architecture = settings.architecture or settings.default_architecture;
+    dynamic_library.architecture = architecture;
     return dynamic_library;
 end
 
--- src/sweet/build_tool/lua/build/cc/StaticLibrary.lua
+++ src/sweet/build_tool/lua/build/cc/StaticLibrary.lua
@@ -1,8 +1,7 @@
 
 local StaticLibrary = build:TargetPrototype( "StaticLibrary" );
 
-local function default_identifier_filename( identifier, platform, architecture, settings )
-    local settings = settings or build:current_settings();
+local function default_identifier_filename( settings, identifier, architecture )
     local identifier = build:absolute( build:interpolate(identifier, settings) );
     local basename = build:basename( identifier );
     local branch = build:branch( identifier );
@@ -11,13 +10,15 @@ local function default_identifier_filename( identifier, platform, architecture, 
 end
 
 function StaticLibrary.create( build, settings, identifier, architecture )
-    local identifier, filename = default_identifier_filename( identifier, platform, architecture, settings );
+    local settings = settings or build:current_settings();
+    local architecture = architecture or settings.architecture or settings.default_architecture;
+    local identifier, filename = default_identifier_filename( settings, identifier, architecture );
     local static_library = build:Target( identifier, StaticLibrary );
     static_library:set_filename( filename );
     static_library:set_cleanable( true );
     static_library:add_ordering_dependency( build:Directory(build:branch(static_library)) );
     static_library.settings = settings;
-    static_library.architecture = settings.architecture or settings.default_architecture;
+    static_library.architecture = architecture;
     return static_library;
 end
 
