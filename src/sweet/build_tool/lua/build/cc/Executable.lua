d3543738b8dc3a33b905bca0b51fe14143346324
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-07-03 20:55:39 +1200

    Use filename as identifier for Executable targets
    This allows target prototypes that depend on executables that are built within the same source tree to find the corresponding target in the dependency graph directly.  If executables used a platform independent identifier then depending target prototypes would need a special case for executables that were built in the same dependency graph.

--- src/sweet/build_tool/lua/build/cc/Executable.lua
+++ src/sweet/build_tool/lua/build/cc/Executable.lua
@@ -1,20 +1,8 @@
 
 local Executable = build:TargetPrototype( "Executable" );
 
-local function default_identifier_filename( identifier, settings )
-    local settings = settings or build:current_settings();
-    local identifier = build:absolute( build:interpolate(identifier, settings) );
-    local basename = build:basename( identifier );
-    local branch = build:branch( identifier );
-    local filename = ("%s/%s"):format( branch, settings.exe_name(basename) );
-    return identifier, filename;
-end
-
 function Executable.create( build, settings, identifier )
-    local identifier, filename = default_identifier_filename( identifier, settings );
-    local executable = build:Target( identifier, Executable );
-    executable:set_filename( filename );
-    executable:set_cleanable( true );
+    local executable = build:File( settings.exe_name(identifier), Executable, settings );
     executable:add_ordering_dependency( build:Directory(build:branch(executable)) );
     executable.settings = settings;
     executable.architecture = settings.architecture or settings.default_architecture;
