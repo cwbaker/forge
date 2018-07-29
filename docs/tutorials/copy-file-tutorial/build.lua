e5e97a8d06afaf7df01f378a1585e698842af754
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-07-29 20:51:35 +1200

    Remove duplicate Copy target prototype definition from copy files example

--- docs/tutorials/copy-file-tutorial/build.lua
+++ docs/tutorials/copy-file-tutorial/build.lua
@@ -1,21 +1,8 @@
 
--- Load the build module containing defaults, initializations, utility
--- functions and targets.
-require "build";
-
--- Define the `Copy` target prototype that expresses a copied file in the
--- dependency graph.  This makes the callable `Copy` target prototype 
--- available in the Lua-based DSL used to specify dependency graphs.
-local Copy = build:TargetPrototype( 'Copy' );
-
-function Copy.create( build, settings, identifier )
-    return build:File( identifier, Copy );
-end
-
-function Copy.build( build, target )
-    build:rm( target );
-    build:cp( target, target:dependency() );
-end
+-- Load the build module and `Copy` target prototype used to express copied
+-- files in the Lua-based DSL.
+require 'build';
+require 'build.Copy';
 
 -- Initialize the build with default settings before use.  Project 
 -- specific settings can be passed in to override the defaults.
