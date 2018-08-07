cd4c8525e62975351fa911dc273c94d54f250c11
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-07 20:34:17 +1200

    Fix Lua compilation in macOS bootstrap script

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -7,7 +7,7 @@ cc() {
     for file in $1; do
         echo $file...
         local DEFINES="-DBUILD_OS_MACOS -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
-        local INCLUDE_DIRS="-I $SRC -I $SRC/lua -I $SRC/boost"
+        local INCLUDE_DIRS="-I $SRC -I $SRC/lua/src -I $SRC/boost"
         local FLAGS="-x c -g"
         clang $DEFINES $INCLUDE_DIRS $FLAGS -o $file.o -c $file
     done
@@ -17,7 +17,7 @@ cxx() {
     for file in $1; do
         echo $file...
         local DEFINES="-DBUILD_OS_MACOS -DBUILD_VARIANT_DEBUG -DBUILD_VERSION=\"bootstrap\" -DLUA_USE_POSIX -DLUA_USE_DLOPEN"
-        local INCLUDE_DIRS="-I $SRC -I $SRC/lua -I $SRC/boost"
+        local INCLUDE_DIRS="-I $SRC -I $SRC/lua/src -I $SRC/boost"
         local FLAGS="-x c++ -std=c++11 -stdlib=libc++ -fexceptions -frtti -g"
         clang++ $DEFINES $INCLUDE_DIRS $FLAGS -o $file.o -c $file
     done
@@ -34,7 +34,7 @@ link() {
 mkdir -p $LIB
 echo boost_system; pushd $SRC/boost/libs/system/src; cxx "*.cpp"; archive $LIB/libboost_system.a; popd
 echo boost_filesystem; pushd $SRC/boost/libs/filesystem/src; cxx "*.cpp"; archive $LIB/libboost_filesystem.a; popd
-echo lua; pushd $SRC/lua; cc '*.c'; archive $LIB/liblua.a; popd
+echo lua; pushd $SRC/lua/src; cc '*.c'; archive $LIB/liblua.a; popd
 echo assert; pushd $SRC/assert; cxx '*.cpp'; archive $LIB/libassert.a; popd
 echo forge; pushd $SRC/forge; cxx '*.cpp'; archive $LIB/libforge.a; popd
 echo forge/forge_lua; pushd $SRC/forge/forge_lua; cxx '*.cpp'; archive $LIB/libforge_lua.a; popd
