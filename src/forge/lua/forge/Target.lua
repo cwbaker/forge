a274da1dd2f79589654e0cde47a0f8801a67166c
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-18 15:43:33 +1200

    Remove target call and depend metamethods from Lua scripts

--- src/forge/lua/forge/Target.lua
+++ src/forge/lua/forge/Target.lua
@@ -1,30 +1,12 @@
 
 local Target = forge.Target;
 
-setmetatable( Target, {
-    __call = function( _, forge, identifier, target_prototype )
-        local target = forge:target( identifier, target_prototype );
-        getmetatable( target ).__call = function( target, ... )
-            local depend_function = target.depend;
-            depend_function( target.forge, target, ... );
-            return target;
-        end;
-        target.forge = forge;
-        return target;
-    end
-} );
-
 function Target.depend( forge, target, dependencies )
+    assert( type(dependencies) == 'table', 'Table.depend() parameter not a table as expected' );
+    forge:merge( target, dependencies );
     local settings = target.settings;
-    if type(dependencies) == 'string' then
-        local source_file = forge:SourceFile( dependencies, settings );
+    for _, value in ipairs(dependencies) do 
+        local source_file = forge:SourceFile( value, settings );
         target:add_dependency( source_file );
-    elseif type(dependencies) == 'table' then
-        forge:merge( target, dependencies );
-        for _, value in ipairs(dependencies) do 
-            local source_file = forge:SourceFile( value, settings );
-            target:add_dependency( source_file );
-        end
     end
-    return target;
 end
