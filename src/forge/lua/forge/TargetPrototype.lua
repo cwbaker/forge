ac055076d9fe4f76a40afb1cb80ab181332d2565
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-19 13:22:29 +1200

    Remove split modules support from target prototypes

--- src/forge/lua/forge/TargetPrototype.lua
+++ src/forge/lua/forge/TargetPrototype.lua
@@ -3,23 +3,6 @@ local TargetPrototype = forge.TargetPrototype;
 
 setmetatable( TargetPrototype, {
     __call = function( _, forge, identifier )
-        -- Split a '.' delimited string into a table hierarchy returning the last 
-        -- level table and identifier.
-        local function split_modules( module, qualified_id )
-            local start = qualified_id:find( ".", 1, true );
-            if start then
-                local identifier = qualified_id:sub( 1, start - 1 );
-                local submodule = module[identifier];
-                if submodule == nil then 
-                    submodule = {};
-                    module[identifier] = submodule;
-                end
-                local remaining = qualified_id:sub( start + 1 );
-                return split_modules( submodule, remaining );
-            end
-            return module, qualified_id;
-        end
-
         local function create( forge, target_prototype, identifier )
             local settings = forge:current_settings();
             local target = forge:File( identifier, target_prototype, settings );
@@ -45,8 +28,6 @@ setmetatable( TargetPrototype, {
             return target;
         end;
 
-        local module, identifier = split_modules( forge, identifier );
-        module[identifier] = target_prototype;
         forge[identifier] = target_prototype;
         return target_prototype;
     end
