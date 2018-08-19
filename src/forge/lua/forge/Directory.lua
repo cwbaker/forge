441b316571e4857f8f2973d8b37cc2ff68ec94fe
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-19 14:11:14 +1200

    Create Directory targets with forge:Target() to avoid infinite recursion through forge:File()

--- src/forge/lua/forge/Directory.lua
+++ src/forge/lua/forge/Directory.lua
@@ -2,7 +2,10 @@
 local Directory = forge:TargetPrototype( "Directory" );
 
 function Directory.create( forge, settings, identifier )
-	return forge:File( identifier, Directory );
+    local target = forge:Target( forge:interpolate(identifier, settings), Directory );
+    target:set_filename( target:path() );
+    target:set_cleanable( true );
+    return target;
 end
 
 function Directory.build( forge, target )
