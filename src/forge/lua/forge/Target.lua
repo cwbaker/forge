e90bb3d678816b17b4a7cc25bfb56ce90179b6d5
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-20 17:15:22 +1200

    Add Target.create() and redirect calls to Target there

--- src/forge/lua/forge/Target.lua
+++ src/forge/lua/forge/Target.lua
@@ -1,6 +1,12 @@
 
 local Target = forge.Target;
 
+function Target.create( forge, identifier, target_prototype )
+	local target = forge:target( identifier, target_prototype );
+	target.settings = forge.settings;
+    return target;	
+end
+
 function Target.depend( forge, target, dependencies )
     assert( type(dependencies) == 'table', 'Table.depend() parameter not a table as expected' );
     forge:merge( target, dependencies );
@@ -9,4 +15,8 @@ function Target.depend( forge, target, dependencies )
         local source_file = forge:SourceFile( value, settings );
         target:add_dependency( source_file );
     end
+end
+
+getmetatable( Target ).__call = function( _, forge, identifier, target_prototype, ... )
+    return Target.create( forge, identifier, target_prototype, ... );
 end
