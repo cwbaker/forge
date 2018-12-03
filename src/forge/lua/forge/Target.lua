f9310b19d59cc085ec916abbe0575149fdcf2951
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-12-03 20:24:41 +1300

    Fix typo in Target.depend() dependencies not table error message

--- src/forge/lua/forge/Target.lua
+++ src/forge/lua/forge/Target.lua
@@ -8,7 +8,7 @@ function Target.create( forge, identifier, target_prototype )
 end
 
 function Target.depend( forge, target, dependencies )
-    assert( type(dependencies) == 'table', 'Table.depend() parameter not a table as expected' );
+    assert( type(dependencies) == 'table', 'Target.depend() parameter not a table as expected' );
     forge:merge( target, dependencies );
     local settings = target.settings;
     for _, value in ipairs(dependencies) do 
