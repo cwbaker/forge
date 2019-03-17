4b13ed484497f9f959907988695845a671a9f2d6
Charles Baker <charles.baker@sweetsoftware.co.nz>
2019-03-17 13:10:05 +1300

    Collect entitlements and provisioning profile implicitly in App target prototype

--- src/forge/lua/forge/cc/xcode/App.lua
+++ src/forge/lua/forge/cc/xcode/App.lua
@@ -8,37 +8,32 @@ function App.create( forge, identifier )
     return app;
 end
 
-function App.depend( forge, target, dependencies )
-    local settings = forge.settings;
-    local entitlements = dependencies.entitlements;
-    if entitlements then 
-        table.insert( dependencies, entitlements );
-        target.entitlements = entitlements:filename();
-        dependencies.entitlements = nil;
-    end
-    return forge.Target.depend( forge, target, dependencies );
-end
-
 function App.build( forge, target )
-    local settings = forge.settings;
-    local xcrun = settings.xcode.xcrun;
-    if target.settings.generate_dsym_bundle then 
-        local executable;
-        for _, dependency in target:dependencies() do 
-            if dependency:prototype() == forge.Lipo then 
-                executable = dependency:filename();
-                break;
-            end
-        end
-        if executable then 
-            forge:system( xcrun, ('xcrun dsymutil -o "%s.dSYM" "%s"'):format(target:ordering_dependency(), executable) );
-            if target.settings.strip then 
-                forge:system( xcrun, ('xcrun strip "%s"'):format(executable) );
-            end
+    local executable;
+    local entitlements;
+    local provisioning_profile;
+
+    for _, dependency in target:dependencies() do
+        local identifier = dependency:id();
+        local prototype = dependency:prototype();
+        if prototype == forge.Lipo then 
+            executable = dependency:filename();
+        elseif identifier == 'Entitlements.xml' then 
+            entitlements = dependency;
+        elseif forge:extension(identifier) == '.mobileprovision' then
+            provisioning_profile = _G.provisioning_profile or dependency;
         end
     end
 
-    local provisioning_profile = _G.provisioning_profile or target.provisioning_profile;
+    local settings = forge.settings;
+    if settings.generate_dsym_bundle and executable then 
+        local xcrun = settings.xcode.xcrun;
+        forge:system( xcrun, ('xcrun dsymutil -o "%s.dSYM" "%s"'):format(target:ordering_dependency(), executable) );
+        if target.settings.strip then 
+            forge:system( xcrun, ('xcrun strip "%s"'):format(executable) );
+        end
+    end
+
     if provisioning_profile then
         local embedded_provisioning_profile = ("%s/embedded.mobileprovision"):format( target:ordering_dependency() );
         forge:rm( embedded_provisioning_profile );
@@ -53,7 +48,7 @@ function App.build( forge, target )
         "-vv";
         ('"%s"'):format( target:ordering_dependency() );
     };
-    local entitlements = target.entitlements;
+
     if entitlements then 
         table.insert( command_line, ('--entitlements "%s"'):format(entitlements) );
     end
