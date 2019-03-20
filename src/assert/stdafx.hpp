1c2fa043899fbb38df25bb573d1e8008817274bf
Charles Baker <charles.baker@sweetsoftware.co.nz>
2019-03-20 20:31:09 +1300

    Remove definitions of _WIN32_WINNT from assert, forge and process

--- src/assert/stdafx.hpp
+++ src/assert/stdafx.hpp
@@ -5,5 +5,3 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #define _SCL_SECURE_NO_DEPRECATE
 #define NOMINMAX
-#define _WIN32_WINNT 0x500
-#define WINVER       0x500
--- src/process/stdafx.hpp
+++ src/process/stdafx.hpp
@@ -1,7 +1,6 @@
 
 #pragma once
 
-#define _WIN32_WINNT 0x0501
 #define WIN32_LEAN_AND_MEAN
 #define _CRT_SECURE_NO_DEPRECATE
 #define _SCL_SECURE_NO_DEPRECATE
