49774eef6d789a02f93ed7a54781d54b47da26a5
Charles Baker <charles.baker@sweetsoftware.co.nz>
2019-07-23 20:45:29 +1200

    Only process a single dependency in Generate build

--- src/forge/lua/forge/Generate.lua
+++ src/forge/lua/forge/Generate.lua
@@ -1,18 +1,16 @@
 
-local Generate = forge:FilePrototype( "Generate" );
+local Generate = forge:FilePrototype( 'Generate' );
 
 function Generate.build( toolset, target )
-    local outputs = {};
-    for _, dependency in target:dependencies() do 
-        local template = assert( loadfile(native(absolute(dependency))) );
-        local success, output_or_error_message = pcall( template, toolset, target );
-        assert( success, output_or_error_message );
-        table.insert( outputs, output_or_error_message );
-    end
+    local dependency = target:dependency();
+    local template = assert( loadfile(native(absolute(dependency))) );
+    local success, output_or_error_message = pcall( template, toolset, target );
+    assert( success, output_or_error_message );
+    local output = output_or_error_message;
 
-    local output_file, error_message = io.open( target:filename(), "wb" );
+    local output_file, error_message = io.open( target:filename(), 'wb' );
     assert( output_file, error_message );
-    output_file:write( table.concat(outputs, "") );
+    output_file:write( output );
     output_file:close();
     output_file = nil;
 end
