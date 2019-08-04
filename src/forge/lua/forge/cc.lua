a3ee42426a7a83bf5f2bb1857ab3aefd2a2e2da1
Charles Baker <charles.baker@sweetsoftware.co.nz>
2019-08-04 18:50:05 +1200

    Move cc module into its own directory

--- src/forge/lua/forge/cc.lua
+++
@@ -1,8 +0,0 @@
-
-if operating_system() == 'windows' then
-    return require 'forge.msvc';
-elseif operating_system() == 'linux' then 
-    return require 'forge.gcc';
-elseif operating_system() == 'macos' then
-    return require 'forge.clang';
-end
---
+++ src/forge/lua/forge/cc/init.lua
@@ -0,0 +1,9 @@
+
+local operating_system = _G.operating_system();
+if operating_system == 'windows' then
+    return require 'forge.cc.msvc';
+elseif operating_system == 'linux' then 
+    return require 'forge.cc.gcc';
+elseif operating_system == 'macos' then
+    return require 'forge.cc.clang';
+end
