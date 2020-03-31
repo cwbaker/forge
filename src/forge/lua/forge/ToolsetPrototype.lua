fa30e2e4cb37093fce3716ccec9e127ec8db9235
Charles Baker <charles.baker@sweetsoftware.co.nz>
2020-03-31 16:39:36 +1300

    Add ToolsetPrototype to replace remaining Module functionality

---
+++ src/forge/lua/forge/ToolsetPrototype.lua
@@ -0,0 +1,28 @@
+
+local Toolset = require 'forge.Toolset';
+local ToolsetPrototype = {};
+
+setmetatable( ToolsetPrototype, {
+    __call = function( ToolsetPrototype, _forge, id )
+        local toolset_prototype = {
+            id = id;
+        };
+        local toolset_metatable = {
+            __index = toolset_prototype;
+        };
+        local toolset_prototype_metatable = {
+            __call = function( module, settings )
+                local toolset = require( 'forge' ):clone( settings );
+                toolset.prototype = toolset_prototype;
+                setmetatable( toolset, toolset_metatable );
+                toolset:install();
+                return toolset;
+            end;
+            __index = Toolset;
+        };
+        setmetatable( toolset_prototype, toolset_prototype_metatable );
+        return toolset_prototype;
+    end;
+} );
+
+return ToolsetPrototype;
