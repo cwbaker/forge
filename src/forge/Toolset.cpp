e45ef1224263fb3187265fa44f7495b73f350d8b
Charles Baker <charles.baker@sweetsoftware.co.nz>
2020-04-03 15:01:55 +1300

    Add Toolset.[ch]pp

---
+++ src/forge/Toolset.cpp
@@ -0,0 +1,45 @@
+//
+// Toolset.cpp
+// Copyright (c) Charles Baker. All rights reserved.
+//
+
+#include "Toolset.hpp"
+#include "ToolsetPrototype.hpp"
+#include "Graph.hpp"
+#include "Forge.hpp"
+#include <assert/assert.hpp>
+
+using namespace sweet;
+using namespace sweet::forge;
+
+Toolset::Toolset( const std::string& id, ToolsetPrototype* toolset_prototype, Graph* graph )
+: id_( id ),
+  graph_( graph ),
+  prototype_( toolset_prototype )
+{
+    SWEET_ASSERT( !id_.empty() );
+    SWEET_ASSERT( graph_ );
+}
+
+Toolset::~Toolset()
+{
+    if ( graph_ )
+    {
+        graph_->forge()->destroy_toolset_lua_binding( this );
+    }
+}
+
+const std::string& Toolset::id() const
+{
+    return id_;
+}
+
+Graph* Toolset::graph() const
+{
+    return graph_;
+}
+
+ToolsetPrototype* Toolset::prototype() const
+{
+    return prototype_;
+}
