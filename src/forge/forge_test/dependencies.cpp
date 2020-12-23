d08d8c6e18311be7910c17130960d8026574cc64
Charles Baker <charles.baker@sweetsoftware.co.nz>
2020-12-24 07:50:10 +1300

    Add FileChecker and Lua bindings to dependencies.cpp

--- src/forge/forge_test/dependencies.cpp
+++ src/forge/forge_test/dependencies.cpp
@@ -4,11 +4,13 @@
 //
 
 #include "stdafx.hpp"
+#include "FileChecker.hpp"
 #include <forge/Forge.hpp>
 #include <forge/ForgeEventSink.hpp>
 #include <error/ErrorPolicy.hpp>
 #include <luaxx/luaxx_unit/LuaUnitTest.hpp>
 #include <assert/assert.hpp>
+#include <lua.hpp>
 #include <UnitTest++/UnitTest++.h>
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
@@ -27,6 +29,7 @@ SUITE( dependencies )
         error::ErrorPolicy* error_policy_;
         forge::Forge* forge_;
         luaxx::LuaUnitTest* lua_unit_test_;
+        FileChecker* file_checker_;
     
         LuaTest()
         {
@@ -34,16 +37,32 @@ SUITE( dependencies )
             error_policy_ = new error::ErrorPolicy;
             forge_ = new forge::Forge( path.string(), *error_policy_, this );
             lua_unit_test_ = new LuaUnitTest( forge_->lua_state(), error_policy_ );
+            file_checker_ = new FileChecker;
+
             forge_->set_root_directory( path.generic_string() );
             forge_->set_stack_trace_enabled( true );
             forge_->set_package_path(
                 forge_->root("../lua/?.lua").generic_string() + ";" +
                 forge_->root("../lua/?/init.lua").generic_string()
             );
+
+            static const luaL_Reg file_functions [] = 
+            {
+                { "create", &LuaTest::create },
+                { "remove", &LuaTest::remove },
+                { "touch", &LuaTest::touch },
+                { nullptr, nullptr }
+            };
+            lua_State* lua_state = forge_->lua_state();
+            lua_pushglobaltable( lua_state );
+            lua_pushlightuserdata( lua_state, file_checker_ );
+            luaL_setfuncs( lua_state, file_functions, 1 );    
+            lua_pop( lua_state, 1 );
         }
 
         ~LuaTest()
         {
+            delete file_checker_;
             delete lua_unit_test_;
             delete forge_;
             delete error_policy_;
@@ -74,6 +93,43 @@ SUITE( dependencies )
             fputs( ".\n", stderr );
             fflush( stderr );
         }
+
+        static int create( lua_State* lua_state )
+        {
+            const int LUA_TEST = lua_upvalueindex( 1 );
+            const int FILENAME = 1;
+            const int TIMESTAMP = 2;
+            const int CONTENT = 3;
+            FileChecker* file_checker = (FileChecker*) lua_touserdata( lua_state, LUA_TEST );
+            std::time_t timestamp = luaL_optinteger( lua_state, TIMESTAMP, 0 );
+            const char* filename = luaL_tolstring( lua_state, FILENAME, nullptr );
+            const char* content = luaL_optstring( lua_state, CONTENT, "" );
+            file_checker->create( filename, content, timestamp );
+            return 0;
+        }
+
+        static int remove( lua_State* lua_state )
+        {
+            const int LUA_TEST = lua_upvalueindex( 1 );
+            const int FILENAME = 1;
+            FileChecker* file_checker = (FileChecker*) lua_touserdata( lua_state, LUA_TEST );
+            const char* filename = luaL_tolstring( lua_state, FILENAME, nullptr );
+            file_checker->remove( filename );
+            return 0;
+        }
+
+        static int touch( lua_State* lua_state )
+        {
+            const int LUA_TEST = lua_upvalueindex( 1 );
+            const int FILENAME = 1;
+            const int TIMESTAMP = 2;
+            FileChecker* file_checker = (FileChecker*) lua_touserdata( lua_state, LUA_TEST );
+            std::time_t timestamp = luaL_optinteger( lua_state, TIMESTAMP, 0 );
+            const char* filename = luaL_tolstring( lua_state, FILENAME, nullptr );
+            file_checker->touch( filename, timestamp );
+            return 0;
+        }
+
     };
 
     TEST_FIXTURE( LuaTest, transitive_dependencies )
