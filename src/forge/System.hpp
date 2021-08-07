3be0ddd2f16369a3beb849767db1c90ead7ca7dc
Charles Baker <charlesbaker400@gmail.com>
2021-08-08 09:43:15 +1200

    LINT: Reduce indentation of declarations in System.hpp

--- src/forge/System.hpp
+++ src/forge/System.hpp
@@ -19,27 +19,27 @@ class System
 {
     float initial_tick_count_; ///< The tick count when this System object was created.
 
-    public:
-        System();
-        
-        bool exists( const std::string& path ) const;
-        bool is_file( const std::string& path ) const;
-        bool is_directory( const std::string& path ) const;
-        bool is_regular( const std::string& path ) const;
-        std::time_t last_write_time( const std::string& path ) const;
-        boost::filesystem::directory_iterator ls( const std::string& path ) const;
-        boost::filesystem::recursive_directory_iterator find( const std::string& path ) const;
-        std::string executable() const;
-        std::string home() const;
-        void mkdir( const std::string& path ) const;
-        void rmdir( const std::string& path ) const;
-        void cp( const std::string& from, const std::string& to ) const;
-        void rm( const std::string& path ) const;
-        const char* operating_system() const;
-        const char* getenv( const char* name ) const;
-        int number_of_logical_processors() const;
-        void sleep( float milliseconds ) const;
-        float ticks() const;
+public:
+    System();
+    
+    bool exists( const std::string& path ) const;
+    bool is_file( const std::string& path ) const;
+    bool is_directory( const std::string& path ) const;
+    bool is_regular( const std::string& path ) const;
+    std::time_t last_write_time( const std::string& path ) const;
+    boost::filesystem::directory_iterator ls( const std::string& path ) const;
+    boost::filesystem::recursive_directory_iterator find( const std::string& path ) const;
+    std::string executable() const;
+    std::string home() const;
+    void mkdir( const std::string& path ) const;
+    void rmdir( const std::string& path ) const;
+    void cp( const std::string& from, const std::string& to ) const;
+    void rm( const std::string& path ) const;
+    const char* operating_system() const;
+    const char* getenv( const char* name ) const;
+    int number_of_logical_processors() const;
+    void sleep( float milliseconds ) const;
+    float ticks() const;
 };
 
 }
