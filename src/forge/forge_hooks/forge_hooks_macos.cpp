f16d82def8bd9ff9b95dac03471765d01d831cd0
Charles Baker <charles@sweetsoftware.nz>
2023-11-20 16:11:05 +1300

    Return full path of accessed files to Forge from macOS hooks

--- src/forge/forge_hooks/forge_hooks_macos.cpp
+++ src/forge/forge_hooks/forge_hooks_macos.cpp
@@ -7,12 +7,58 @@
 #include <string.h>
 #include <sys/uio.h>
 #include <sys/stat.h>
+#include <limits.h>
 
 #define DYLD_INTERPOSE(_replacement,_replacee) \
     __attribute__((used)) static struct{ const void* replacement; const void* replacee; } _interpose_##_replacee \
     __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
 
+namespace
+{
+
 static const int FILE_DESCRIPTOR = 3;
+
+static void log_open( int fd, int oflag )
+{
+    if ( fd >= 0 )
+    {
+        struct stat stat;
+        if ( fstat(fd, &stat) == 0 && (stat.st_mode & S_IFREG) != 0 ) 
+        {
+            char path [PATH_MAX];
+            ssize_t size = fcntl( fd, F_GETPATH, path );
+            if ( size >= 0 && size_t(size) < sizeof(path) )
+            {
+                path[size] = 0;
+                const bool read_only = (oflag & (O_WRONLY | O_RDWR)) == 0;
+                if ( read_only )
+                {
+                    struct iovec iovecs[] =
+                    {
+                        { (void*) "== read '", 9 },
+                        { (void*) path, strlen(path) },
+                        { (void*) "'\n", 2 }
+                    };
+                    size_t written = writev( FILE_DESCRIPTOR, iovecs, 3 );
+                    (void) written;
+                }
+                else
+                {
+                    struct iovec iovecs[] =
+                    {
+                        { (void*) "== write '", 10 },
+                        { (void*) path, strlen(path) },
+                        { (void*) "'\n", 2 }
+                    };
+                    size_t written = writev( FILE_DESCRIPTOR, iovecs, 3 );
+                    (void) written;
+                }
+            }
+        }
+    }
+}
+
+}
 
 extern "C" 
 {
@@ -35,30 +81,7 @@ int open_interpose( const char* filename, int oflag, ... )
 
     if ( fd >= 0 )
     {
-        struct stat stat;
-        if ( fstat(fd, &stat) == 0 && (stat.st_mode & S_IFREG) != 0 ) 
-        {
-            if ( (oflag & (O_WRONLY | O_RDWR)) == 0 )
-            {
-                struct iovec iovecs[] =
-                {
-                    { (void*) "== read '", 9 },
-                    { (void*) filename, strlen(filename) },
-                    { (void*) "'\n", 2 }
-                };
-                writev( FILE_DESCRIPTOR, iovecs, 3 );
-            }
-            else
-            {
-                struct iovec iovecs[] =
-                {
-                    { (void*) "== write '", 10 },
-                    { (void*) filename, strlen(filename) },
-                    { (void*) "'\n", 2 }
-                };
-                writev( FILE_DESCRIPTOR, iovecs, 3 );
-            }
-        }
+        log_open( fd, oflag );
     }
 
     return fd;
@@ -85,30 +108,7 @@ int open$NOCANCEL_interpose( const char* filename, int oflag, ... )
 
     if ( fd >= 0 )
     {
-        struct stat stat;
-        if ( fstat(fd, &stat) == 0 && (stat.st_mode & S_IFREG) != 0 ) 
-        {
-            if ( (oflag & (O_WRONLY | O_RDWR)) == 0 )
-            {
-                struct iovec iovecs[] =
-                {
-                    { (void*) "== read '", 9 },
-                    { (void*) filename, strlen(filename) },
-                    { (void*) "'\n", 2 }
-                };
-                writev( FILE_DESCRIPTOR, iovecs, 3 );
-            }
-            else
-            {
-                struct iovec iovecs[] =
-                {
-                    { (void*) "== write '", 10 },
-                    { (void*) filename, strlen(filename) },
-                    { (void*) "'\n", 2 }
-                };
-                writev( FILE_DESCRIPTOR, iovecs, 3 );
-            }
-        }
+        log_open( fd, oflag );
     }
 
     return fd;
