6e4ad35edb2c1bbb2d5a7bf0947b1e055a60b4bb
Charles Baker <charles@sweetsoftware.nz>
2026-05-25 11:29:21 +1200

    LINT: Remove <dlfcn.h> and trailing whitespace from forge_hooks_macos.cpp

--- src/forge/forge_hooks/forge_hooks_macos.cpp
+++ src/forge/forge_hooks/forge_hooks_macos.cpp
@@ -1,7 +1,6 @@
 
 #include <unistd.h>
 #include <fcntl.h>
-#include <dlfcn.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
@@ -23,7 +22,7 @@ static void log_open( int fd, int oflag )
     if ( fd >= 0 )
     {
         struct stat stat;
-        if ( fstat(fd, &stat) == 0 && (stat.st_mode & S_IFREG) != 0 ) 
+        if ( fstat(fd, &stat) == 0 && (stat.st_mode & S_IFREG) != 0 )
         {
             char path [PATH_MAX];
             int result = fcntl( fd, F_GETPATH, path );
@@ -59,7 +58,7 @@ static void log_open( int fd, int oflag )
 
 }
 
-extern "C" 
+extern "C"
 {
 
 int open_interpose( const char* filename, int oflag, ... )
