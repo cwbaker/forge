b60b6cc4258d8b02e3c023d6ee90f240a88ccaee
Charles Baker <charles@sweetsoftware.nz>
2026-05-28 20:51:09 +1200

    Launch all of the Lua unit tests from lua_tests.cpp

---
+++ src/forge/forge_test/lua_tests.cpp
@@ -0,0 +1,49 @@
+//
+// lua_tests.cpp
+// Copyright (c) Charles Baker. All rights reserved.
+//
+
+#include "ForgeLuaFixture.hpp"
+#include <forge/Forge.hpp>
+#include <UnitTest++/UnitTest++.h>
+
+using namespace sweet::forge;
+
+SUITE( lua_tests )
+{
+    TEST_FIXTURE( ForgeLuaFixture, path_tests )
+    {
+        int errors = forge->file( "path_tests.lua" );
+        CHECK( errors == 0 );
+    }
+
+    TEST_FIXTURE( ForgeLuaFixture, string_tests )
+    {
+        int errors = forge->file( "string_tests.lua" );
+        CHECK( errors == 0 );
+    }
+
+    TEST_FIXTURE( ForgeLuaFixture, file_system_tests )
+    {
+        int errors = forge->file( "file_system_tests.lua" );
+        CHECK( errors == 0 );
+    }
+
+    TEST_FIXTURE( ForgeLuaFixture, hooks )
+    {
+        int errors = forge->file( "hooks_tests.lua" );
+        CHECK( errors == 0 );
+    }
+
+    TEST_FIXTURE( ForgeLuaFixture, preorder )
+    {
+        int errors = forge->file( "preorder.lua" );
+        CHECK_EQUAL( 4, errors );
+    }
+
+    TEST_FIXTURE( ForgeLuaFixture, transitive_dependencies )
+    {
+        int errors = forge->file( "transitive_dependencies.lua" );
+        CHECK( errors == 0 );
+    }
+}
